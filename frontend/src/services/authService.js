4f51918c280899ee10fc069f2257c5683c160975
Hardik Patel <h.patel290695@gmail.com>
2024-02-01 20:07:19 -0500

    service change

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth/";
+const API_URL = "http://localhost:5000/";
 
 const register = (username, email, password, role, profileInfo) => {
   return axios.post(API_URL + "register", {
