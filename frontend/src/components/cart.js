20b4b2061b044f430ca0ba3cac326cf0e8b1828d
Hardik Patel <h.patel290695@gmail.com>
2024-12-18 22:47:03 -0500

    Solve bug in cart page

--- frontend/src/components/cart.js
+++ frontend/src/components/cart.js
@@ -41,26 +41,25 @@ const Cart = () => {
     const makePayment = async () => {
         // console.log("Hello")
         const stripe = await loadStripe("pk_test_51P3WBGP3QiX5wZP6swEDPqAfsRzjSBIdnsjn2nDFNe3dFgQOKZK4sVUAVPeog7Sg6krgyZUFI4HwGQs81cpQfdKY00uSGRUl1b")
-    
+
         const body = {
-          userId: userId,
-          products: cartItems
+            userId: userId,
+            products: cartItems
         }
-    
+
         const response = await axios.post("http://localhost:5000/api/createCheckout", body);
-    
+
         const result = stripe.redirectToCheckout({
-          sessionId: response.data.id
+            sessionId: response.data.id
         });
-       
+
         if (result.error) {
-          console.log("err --", result.error);
+            console.log("err --", result.error);
         }
-      }
+    }
 
     return (
-
-        <div style={{ width: '50%', margin: '0 auto' }}>
+        <div style={{ width: '50%', margin: '0 auto', alignItems: "center", display: 'flex', flexDirection: 'column' }}>
             <Typography variant="h5" mt={2}>
                 Shopping Cart
             </Typography>
@@ -71,7 +70,7 @@ const Cart = () => {
                             <CardMedia component='img' image={product.image} sx={{ width: 150, height: 150 }} alt="product image" />
                             <Box sx={{ flexDirection: 'column', display: 'inline-block', width: '80%', verticalAlign: 'top' }}>
                                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                                    <CardContent sx={{  padding: '10px' }}>
+                                    <CardContent sx={{ padding: '10px' }}>
                                         <Typography component="div" variant="h6">{product.productName}</Typography>
                                         <Typography variant="body2" color="text.secondary" component="div">${product.price}</Typography>
                                     </CardContent>
@@ -105,12 +104,12 @@ const Cart = () => {
                     <CustomButton size="medium" variant="contained" onClick={makePayment}>Checkout - ${totalAmount.toFixed(2)}</CustomButton>
                 </>
             }
-            {(cartItems.length == 0) && 
-            <>
-                <Typography variant='h4'> Your Cart is Empty ! </Typography>
-                <CustomButton size="medium" variant="contained" onClick={() => {navigate("/");}}>Continue Shopping</CustomButton>
+            {(cartItems.length == 0) &&
+                <>
+                    <Typography variant='h4'> Your Cart is Empty ! </Typography>
+                    <CustomButton size="medium" variant="contained" onClick={() => { navigate("/"); }}>Continue Shopping</CustomButton>
 
-            </>}
+                </>}
         </div >
     );
 };
